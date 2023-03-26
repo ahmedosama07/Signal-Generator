c507cc1ffda8fc34fb0ce6e1c3cd6824443cc786
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-03-26 15:02:42 +0200

    Update TimeReversal.m

--- TimeReversal.m
+++ TimeReversal.m
@@ -1,6 +1,6 @@
 function [modifiedSignal,operationTime] = TimeReversal(signal,time)
 %TimeReversal Summary of this function goes here
 %   Function reverses the signal in time
-    modifiedSignal = signal;
-    operationTime = -1 .* time;
+    modifiedSignal = signal(end:-1:1);
+    operationTime = time;
 end
